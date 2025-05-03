893cfb96ae05f6ea54aaa07edf7d0dba47e027b3
seell002 <seell002@umn.edu>
2025-05-02 19:22:01 -0500

    Trail 2

--- pinot/query-pinot/most_popular_state.sql
+++ pinot/query-pinot/most_popular_state.sql
@@ -1,10 +1,10 @@
 WITH state_counts AS (
   SELECT
-    s.artistid,
-    addr.state       AS state,
-    COUNT(*)         AS cnt
+    s.artistid,          -- from the Stream table
+    addr.state           AS state,
+    COUNT(*)             AS cnt
   FROM "stream"            AS s
-  JOIN "customer"          AS c    ON c.id        = s.customerid
+  JOIN "customer"          AS c    ON c.id           = s.customerid
   JOIN "address"           AS addr ON addr.customerid = c.id
   GROUP BY
     s.artistid,
@@ -20,10 +20,10 @@ FROM (
     state,
     cnt,
     ROW_NUMBER() OVER (
-      PARTITION BY artistid
+      PARTITION BY artistid  -- for each artist
       ORDER BY cnt DESC
     ) AS rn
   FROM state_counts
 )
-WHERE rn = 1
+WHERE rn = 1               -- top state per artist
 ORDER BY artistid;
